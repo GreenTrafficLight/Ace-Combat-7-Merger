b74c46bf2692edca43cfdd92eaba663530c6e209
GreenTrafficLight <tailgunnerpursuit@gmail.com>
2023-10-01 15:21:50 +0200

    Added dictionary to check for existing SkinNo

--- Ace Combat Merger/DictionaryDataTable.cs
+++ Ace Combat Merger/DictionaryDataTable.cs
@@ -6,10 +6,12 @@ using System.Threading.Tasks;
 
 namespace Ace_Combat_Merger
 {
-    public class DictionaryDataTable
+    public class DictionaryAC7DataTable
     {
         public List<string> RowNames = new List<string>();
 
         public Dictionary<string, List<string>> IDs = new Dictionary<string, List<string>>();
+
+        public Dictionary<string, List<int>> PlaneSkinNoDictionary = new Dictionary<string, List<int>>();
     }
 }
